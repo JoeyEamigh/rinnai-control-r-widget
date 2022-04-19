603d95df3eae4ca781f3661dc5eb3852c37b1a7b
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2022-04-18 22:20:55 -0400

    Initial Commit

---
+++ rinnai-control-r-widget/rinnai_control_r_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  rinnai_control_r_widgetApp.swift
+//  rinnai-control-r-widget
+//
+//  Created by Joey Eamigh on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct rinnai_control_r_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
